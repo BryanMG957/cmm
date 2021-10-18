4da461ad2265b3b256a4519803b8919a0b1b3a81
BryanMG957 <bryanmartinezdev@gmail.com>
2021-10-18 12:05:06 -0400

    lint code

--- who_is_in_space.rb
+++ who_is_in_space.rb
@@ -1,50 +1,49 @@
-require 'httparty'
-require 'json'
+require "httparty"
+require "json"
 
 class Astronaut
+  attr_accessor :craft, :name
 
-	attr_accessor :craft, :name
+  @@astronauts = Array.new
 
-	@@astronauts = Array.new
+  def initialize(craft, name)
+    @craft = craft
+    @name = name
+    @@astronauts << self
+  end
 
-	def initialize(craft, name)
-      @craft = craft
-      @name = name
-      @@astronauts << self
+  def self.fetch_astronauts
+    #API Call
+    response = HTTParty.get("http://api.open-notify.org/astros.json")
+
+    parsed_response = JSON.parse(response.body)
+
+    people_in_space = parsed_response["people"]
+
+    #Create an object for each person in space
+    people_in_space.each do |person|
+      person = Astronaut.new(person["craft"], person["name"])
     end
+  end
 
-    def self.fetch_astronauts
-    	#API Call
-		response = HTTParty.get('http://api.open-notify.org/astros.json')
+  #NOT attempting the challenge with this style of formatting (as per instructions in email) it should be dynamics
+  #TODO improve formatting to use the actual lenght of the name.
+  #TODO
+  def self.to_table(astronauts)
+    table_string = ""
+    table_string += "Name".ljust(18) + " | Craft".ljust(15) + "\n"
+    table_string += "-".ljust(18, "-") + "-".ljust(15, "-") + "\n"
+    astronauts.each { |astronaut|
+      table_string += "#{astronaut.name.ljust(18)} | #{astronaut.craft.ljust(15)} \n"
+    }
 
-		parsed_response = JSON.parse(response.body)
+    table_string
+  end
 
-		people_in_space = parsed_response["people"]
-
-		#Create an object for each person in space
-		people_in_space.each do |person|
-			person = Astronaut.new(person["craft"], person["name"])
-		end
-    end
-
-    #NOT attempting the challenge with this style of formatting (as per instructions in email) it should be dynamics
-	#TODO improve formatting to use the actual lenght of the name.
-	#TODO 
-    def self.to_table(astronauts)
-    	table_string = ""
-    	table_string += "Name".ljust(18) + " | Craft".ljust(15) + "\n"
-    	table_string += "-".ljust(18, "-") + "-".ljust(15, "-") + "\n"
-    	astronauts.each { |astronaut| 
-    		table_string += "#{astronaut.name.ljust(18)} | #{astronaut.craft.ljust(15)} \n"
-    	}
-    	 
-    	table_string
-    end
-
-    def self.all_astronauts
-		fetch_astronauts()
-		to_table(@@astronauts)
-    end
+  def self.all_astronauts
+    fetch_astronauts()
+    to_table(@@astronauts)
+  end
 end
 
 astronauts = Astronaut.all_astronauts()
@@ -68,11 +67,11 @@ Wang Yaping        | Shenzhou 13
 "
 
 def test_table_output(astronauts, expected_output)
-	if expected_output == Astronaut.to_table(astronauts)
-		"Output matches"
-	else
-		"Unexpected Output"
-	end
+  if expected_output == Astronaut.to_table(astronauts)
+    "Output matches"
+  else
+    "Unexpected Output"
+  end
 end
 
 puts test_table_output(test_astronauts, expected_output)
